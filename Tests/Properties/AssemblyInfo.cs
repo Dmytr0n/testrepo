9d00423f1ee1313dfdc76e0304a8141ac5d0a8ef
Dmytro <reserv2017@ukr.net>
2024-11-16 21:20:54 +0200

    delete obj and bin

--- Tests/Properties/AssemblyInfo.cs
+++ Tests/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("testingMenu")]
+[assembly: AssemblyTitle("UnitTestProject1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("testingMenu")]
+[assembly: AssemblyProduct("UnitTestProject1")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("5f602bf5-dfa0-4cfa-8046-4a532b621229")]
+[assembly: Guid("14f887d2-a0f3-4d9a-81fc-8237b748fb37")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
